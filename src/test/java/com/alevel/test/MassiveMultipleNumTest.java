ca9cabed53a5c592dbbbf48367dbf2799f4ab461
unknown <dima4elom@gmail.com>
2019-10-24 21:29:59 +0300

    Add test for MassiveMultipleNum

---
+++ src/test/java/com/alevel/test/MassiveMultipleNumTest.java
@@ -0,0 +1,19 @@
+package com.alevel.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MassiveMultipleNumTest {
+
+    int [] testMassive = new int[]{3,6,13,7,24,1,5,6,21,16,22,11};
+    int number = 3;
+
+    @Test
+    public void multipleNumber(){
+        int [] multiple3Massive = new int[]{3,6,24,6,21};
+        Assert.assertArrayEquals(MassiveMultipleNum.multipleNumber(testMassive,3),multiple3Massive);
+    }
+
+}
